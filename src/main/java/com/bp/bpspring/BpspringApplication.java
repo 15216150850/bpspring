f062ba0f43e6696e16fd2e7b877818e2cf88b5c5
zxk <zxk@zxks-MacBook-Pro.local>
2019-08-28 07:58:48 -0700

    Initial commit

---
+++ src/main/java/com/bp/bpspring/BpspringApplication.java
@@ -0,0 +1,16 @@
+package com.bp.bpspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目启动类，单机版项目框架
+ * @author 钟欣凯
+ */
+@SpringBootApplication
+public class BpspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BpspringApplication.class, args);
+    }
+}
