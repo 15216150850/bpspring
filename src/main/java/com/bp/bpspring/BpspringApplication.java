25fa1c4272dbf090517d3115d607d4154d4f559e
zxk <123@123.com>
2019-08-28 08:07:48 -0700

    ads

--- src/main/java/com/bp/bpspring/BpspringApplication.java
+++ src/main/java/com/bp/bpspring/BpspringApplication.java
@@ -11,6 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BpspringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BpspringApplication.class, args);
+        SpringApplication.run(BpspringApplication.class, args);cdd
     }
 }
